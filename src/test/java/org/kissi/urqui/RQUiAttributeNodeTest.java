12566f055e0b9ad7210778007db4205311e904ab
urqui <jbell@urqui.com>
2018-10-03 13:27:37 -0700

    unittests

---
+++ src/test/java/org/kissi/urqui/RQUiAttributeNodeTest.java
@@ -0,0 +1,112 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2017-2018 ForgeRock AS.
+ */
+
+package org.kissi.urqui;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+import static org.forgerock.json.JsonValue.*;
+import static org.forgerock.json.test.assertj.AssertJJsonValueAssert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.util.Enumeration;
+import java.util.List;
+import java.util.ResourceBundle;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.NameCallback;
+import org.forgerock.json.JsonValue;
+import org.forgerock.openam.auth.node.api.Action;
+import org.forgerock.openam.auth.node.api.ExternalRequestContext.Builder;
+import org.forgerock.openam.auth.node.api.TreeContext;
+import org.forgerock.util.i18n.PreferredLocales;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+
+/**
+ * A node which collects a password from the user via a password callback.
+ *
+ * <p>Places the result in the shared state as 'password'.</p>
+ */
+
+public class RQUiAttributeNodeTest   {
+
+    private RQUiAttributeNode node;
+
+    @BeforeMethod
+    public void before() {
+        initMocks(this);
+        node = new RQUiAttributeNode();
+    }
+
+    @Test
+    public void testProcessWithNoCallbacksReturnsASingleCallback() {
+        // Given
+        JsonValue sharedState = json(object(field("initial", "initial")));
+        PreferredLocales preferredLocales = mock(PreferredLocales.class);
+        ResourceBundle resourceBundle = new MockResourceBundle("RQUi");
+        given(preferredLocales.getBundleInPreferredLocale(any(), any())).willReturn(resourceBundle);
+
+        // When
+        Action result = node.process(getContext(sharedState, preferredLocales, emptyList()));
+
+        // Then
+        assertThat(result.outcome).isEqualTo(null);
+        assertThat(result.callbacks).hasSize(2);
+        assertThat(result.callbacks.get(0)).isInstanceOf(NameCallback.class);
+        assertThat(((NameCallback) result.callbacks.get(0)).getPrompt()).isEqualTo("RQUi");
+        assertThat((Object) result.sharedState).isNull();
+        assertThat(sharedState).isObject().containsExactly(entry("initial", "initial"));
+    }
+
+    @Test
+    public void testProcessWithCallbacksAddsToState() {
+        JsonValue sharedState = json(object(field("initial", "initial")));
+         
+        assertThat(sharedState).isObject().containsExactly(entry("initial", "initial"));
+		
+    }
+
+    private TreeContext getContext(JsonValue sharedState, PreferredLocales preferredLocales,
+                                   List<? extends Callback> callbacks) {
+        return new TreeContext(sharedState, new Builder().locales(preferredLocales).build(), callbacks);
+    }
+
+    static class MockResourceBundle extends ResourceBundle {
+        private final String value;
+
+        MockResourceBundle(String value) {
+            this.value = value;
+        }
+
+        @Override
+        protected Object handleGetObject(String key) {
+            return value;
+        }
+
+        @Override
+        public Enumeration<String> getKeys() {
+            return null;
+        }
+    }
+}
+
